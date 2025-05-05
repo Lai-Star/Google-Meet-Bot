9e781afb4036a89cfc8978aa4f14126ebf3201d0
leonel fernando <leoneelo123@outlook.com>
2025-05-05 21:47:11 +0900

    set the server config

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/api/join-meet', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server is running on http://0.0.0.0:3000");
 });
 
